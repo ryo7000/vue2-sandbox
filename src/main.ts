3a956cf201ddac83d76802d4347f01d7d9b65abc
ryo <ryo7000@gmail.com>
2021-08-30 22:25:45 +0900

    Add vuetify with 'vue add vuetify'

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import VueCompositionApi from "@vue/composition-api"
+import VueCompositionApi from "@vue/composition-api";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
 
 new Vue({
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
